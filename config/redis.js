4f83764a3ecaba1abebbd341c6a60c3e6af52373
sohan paliyal <officialsohanpaliyal@gmail.com>
2026-05-29 15:04:51 +0530

    temporary remove redis

--- config/redis.js
+++ config/redis.js
@@ -1,10 +1,10 @@
 const IORedis = require('ioredis');
 
-const redisConnection = new IORedis(
-  process.env.REDIS_URL,
-   {
-    maxRetriesPerRequest: null
-  }
-);
+// const redisConnection = new IORedis(
+//   process.env.REDIS_URL,
+//    {
+//     maxRetriesPerRequest: null
+//   }
+// );
 
 module.exports = redisConnection;
